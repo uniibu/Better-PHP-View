985bb5a054833a86b64b51ad59d147830116296f
uniibu <unibtc@gmail.com>
2017-04-12 18:45:53 +0000

    First Commit

---
+++ README.md
@@ -0,0 +1,101 @@
+[![Build Status](https://travis-ci.org/geggleto/PHP-View.svg?branch=master)](https://travis-ci.org/geggleto/PHP-View)
+
+## Better-PHP-View
+
+This is a renderer for rendering PHP view scripts into a PSR-7 Response object. It works well with Slim Framework 3.
+
+## This Version
+
+This version is a fork from the original PHP-view https://github.com/slimphp/PHP-View
+This version also adds optional escaping using zend-escaper (https://github.com/zendframework/zend-escaper) and the ability to include other views using $PHPview->include('TEMPLATE NAME');
+Included templates must reside on the same template directory.
+To use the escape function, just add the type of escaping when calling $PHPview->render ex. $PHPview->render($response,'template.php',['args' => 'value'],'html')
+If escape type is left out, nothing will be escaped. It will only escape the values of the associative arrays.
+
+### Cross-site scripting (XSS) risks (optional on this version)
+
+Note that PHP-View has no built-in mitigation from XSS attacks. It is the developer's responsibility to use `htmlspecialchars()` or a component like [zend-escaper](https://github.com/zendframework/zend-escaper). Alternatively, consider  [Twig-View](https://github.com/slimphp/Twig-View).
+
+
+
+## Templates
+You may use `$this` inside your php templates. `$this` will be the actual PhpRenderer object will allow you to render sub-templates
+
+## Installation
+
+Install with [Composer](http://getcomposer.org):
+
+    composer require uniibu/better-php-view
+
+
+## Usage with Slim 3 (this version)
+
+```php
+use Slim\Views\PhpRenderer;
+
+include "vendor/autoload.php";
+
+$app = new Slim\App();
+$container = $app->getContainer();
+$container['renderer'] = new PhpRenderer("./templates");
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->renderer->render($response, "/hello.php", $args, 'html');
+});
+
+$app->run();
+```
+
+## Usage with any PSR-7 Project
+```php
+//Construct the View
+$phpView = new PhpRenderer("./path/to/templates");
+
+//Render a Template
+$response = $phpView->render(new Response(), "/path/to/template.php", $yourData, 'html');
+```
+## Escaping Options (this version) check https://github.com/zendframework/zend-escaper for reference
+
+> 'html' = zend->escapeHtml
+> 'attr' = zend->escapeHtmlAttr
+> 'url' = zend->escapeUrl
+> 'js' = zend->escapeJs
+> 'css' = zend->escapeCss
+
+## Template Variables (this version)
+
+You can now add variables and escape type to your renderer that will be available to all templates you render.
+
+```php
+// via the constructor
+$templateVariables = [
+    "title" => "Title"
+];
+$phpView = new PhpRenderer("./path/to/templates", $templateVariables, 'html');
+
+// or setter
+$phpView->setAttributes($templateVariables);
+
+// or individually
+$phpView->addAttribute($key, $value);
+```
+
+Data passed in via `->render()` takes precedence over attributes.
+```php
+$templateVariables = [
+    "title" => "Title"
+];
+$phpView = new PhpRenderer("./path/to/templates", $templateVariables, 'js');
+
+//...
+
+$phpView->render($response, $template, [
+    "title" => "My Title"
+], 'attr');
+// In the view above, the $title will be "My Title" and not "Title"
+```
+
+## Exceptions
+`\RuntimeException` - if template does not exist
+
+`\InvalidArgumentException` - if $data contains 'template'
---
+++ src/PhpRenderer.php
@@ -0,0 +1,259 @@
+<?php
+/**
+ * Slim Framework (http://slimframework.com)
+ *
+ * @link      https://github.com/slimphp/PHP-View
+ * @copyright Copyright (c) 2011-2015 Josh Lockhart
+ * @license   https://github.com/slimphp/PHP-View/blob/master/LICENSE.md (MIT License)
+ */
+/**
+ * edited to add escaping and includes
+ * All rights belong to the original creator
+ * John Sayo - unibtc@gmail.com
+*/
+namespace Slim\Views;
+
+use \InvalidArgumentException;
+use Psr\Http\Message\ResponseInterface;
+use \Zend\Escaper\Escaper;
+/**
+ * Class PhpRenderer
+ * @package Slim\Views
+ *
+ * Render PHP view scripts into a PSR-7 Response object
+ */
+class PhpRenderer
+{
+    /**
+     * Zend Escaper
+     */
+    public $escaper;
+
+    /**
+     * @var string
+     */
+    protected $templatePath;
+
+    /**
+     * @var array
+     */
+    protected $attributes;
+
+    /**
+     * SlimRenderer constructor.
+     *
+     * @param string $templatePath
+     * @param array $attributes
+     */
+    public function __construct($templatePath = "", $attributes = [])
+    {
+        $this->templatePath = rtrim($templatePath, '/\\') . '/';
+        $this->attributes = $attributes;
+        $this->escaper = new \Zend\Escaper\Escaper('utf-8');
+    }
+
+    /**
+     * Render a template
+     *
+     * $data cannot contain template as a key
+     *
+     * throws RuntimeException if $templatePath . $template does not exist
+     *
+     * @param ResponseInterface $response
+     * @param string             $template
+     * @param array              $data
+     *
+     * @return ResponseInterface
+     *
+     * @throws \InvalidArgumentException
+     * @throws \RuntimeException
+     */
+    public function render(ResponseInterface $response, $template, $data = [], $escape = null)
+    {
+        $output = $this->fetch($template, $data, $escape);
+
+        $response->getBody()->write($output);
+
+        return $response;
+    }
+
+    /**
+     * Get the attributes for the renderer
+     *
+     * @return array
+     */
+    public function getAttributes()
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * Set the attributes for the renderer
+     *
+     * @param array $attributes
+     */
+    public function setAttributes(array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Add an attribute
+     *
+     * @param $key
+     * @param $value
+     */
+    public function addAttribute($key, $value) {
+        $this->attributes[$key] = $value;
+    }
+
+    /**
+     * Retrieve an attribute
+     *
+     * @param $key
+     * @return mixed
+     */
+    public function getAttribute($key) {
+        if (!isset($this->attributes[$key])) {
+            return false;
+        }
+
+        return $this->attributes[$key];
+    }
+
+    /**
+     * Get the template path
+     *
+     * @return string
+     */
+    public function getTemplatePath()
+    {
+        return $this->templatePath;
+    }
+
+    /**
+     * Set the template path
+     *
+     * @param string $templatePath
+     */
+    public function setTemplatePath($templatePath)
+    {
+        $this->templatePath = rtrim($templatePath, '/\\') . '/';
+    }
+
+    public function escape($toescape,$type)
+    {
+
+        $arrHolder = array();
+        foreach ($toescape as $key => $value) {
+            $newarr = array();
+            if(is_array($value)){
+                $value = $this->escape($value,$type);
+                $newarr = $value;
+            }else{
+                    switch ($type) {
+                       case 'html':
+                           $newarr = $this->escaper->escapeHtml($value);
+                           break;
+                       case 'attr':
+                           $newarr = $this->escaper->escapeHtmlAttr($value);
+                           break;
+                        case 'js':
+                           $newarr = $this->escaper->escapeJs($value);
+                           break;
+                        case 'css':
+                           $newarr = $this->escaper->escapeCss($value);
+                           break;
+                        case 'url':
+                           $newarr = $this->escaper->escapeUrl($value);
+                           break;
+                       default:
+                          return $toescape;
+                           break;
+                    }
+
+            }
+           $arrHolder[$key] = $newarr;
+
+        }
+
+        return $arrHolder;
+    }
+
+    /**
+     * Renders a template and returns the result as a string
+     *
+     * cannot contain template as a key
+     *
+     * throws RuntimeException if $templatePath . $template does not exist
+     *
+     * @param $template
+     * @param array $data
+     *
+     * @return mixed
+     *
+     * @throws \InvalidArgumentException
+     * @throws \RuntimeException
+     */
+    public function fetch($template, array $data = [], $escape = null) {
+        if (isset($data['template'])) {
+            throw new \InvalidArgumentException("Duplicate template key found");
+        }
+
+        if (!is_file($this->templatePath . $template)) {
+            throw new \RuntimeException("View cannot render `$template` because the template does not exist");
+        }
+
+        $data = array_merge($this->attributes, $data);
+
+        /**
+         * Expose the merge data to $this->attributes
+         * so that it is available via getAttribute/s
+         */
+        $this->attributes = $data;
+
+        /**
+         * escape data attributes before sending to page
+         * Note: this will escape all values on an associative
+         * array only and not their keys,
+         * also works for nested arrays.
+         */
+        if(!is_null($escape))
+        {
+          $data = $this->escape($data,$escape);
+        }
+        echo '<pre>';
+        echo print_r($data);
+        echo '</pre>';
+         try {
+            ob_start();
+            $this->protectedIncludeScope($this->templatePath . $template, $data);
+            $output = ob_get_contents();
+        } finally {
+            ob_end_clean();
+        }
+
+        return $output;
+    }
+
+    /**
+     * Includes any template before rendering
+     * NOTE: all attributes are also exposed to their
+     * child templates
+     *
+     * @param string $template
+     * @param array $template
+     */
+    public function include ($template) {
+        return include $this->templatePath.$template;
+    }
+    /**
+     * @param string $template
+     * @param array $data
+     */
+    protected function protectedIncludeScope ($template, array $data) {
+        extract($data);
+       include $template;
+
+    }
+}
